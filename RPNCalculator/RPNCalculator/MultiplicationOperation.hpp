f205e7d04bda1a4f974e1fd7efaffeae949268c0
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 13:05:56 -0800

    Implemented perform function

--- RPNCalculator/RPNCalculator/MultiplicationOperation.hpp
+++ RPNCalculator/RPNCalculator/MultiplicationOperation.hpp
@@ -3,5 +3,5 @@ class MultiplicationOperation : public AbstractOperation
 {
 	static const char MULTIPLICATION_CODE = '*';
 	MultiplicationOperation() : AbstractOperation(MULTIPLICATION_CODE) {};
-	
+	int perform(int a, int b) { return a * b; }
 };
