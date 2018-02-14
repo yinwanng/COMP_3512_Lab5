1b87813290cfbdd7e67045221eca7fff194e3b96
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 13:13:40 -0800

    Included header files and data members

---
+++ RPNCalculator/RPNCalculator/RPNCalculator.hpp
@@ -0,0 +1,17 @@
+#include <stack>
+#include "AdditionOperation.hpp"
+#include "SubstractionOperation.hpp"
+#include "DivisionOperation.hpp"
+#include "MultiplicationOperation.hpp"
+
+class RPNCalculator
+{
+public:
+
+private:
+	int result;
+	std::stack<int> stack;
+};
+
+
+
