aef5a1b3f524489fb0ed84b8ccf152d20e1292d3
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 12:21:13 -0800

    Defined a virtual destructor called ~AbstractOperation

--- RPNCalculator/RPNCalculator/AbstractOperation.hpp
+++ RPNCalculator/RPNCalculator/AbstractOperation.hpp
@@ -3,6 +3,7 @@ class AbstractOperation : public Operation
 {
 	AbstractOperation(const char& c) : operation_type(c) {}
 	char get_code() const { return operation_type; }
+	AbstractOperation() {};
 
 private:
 	char operation_type;
