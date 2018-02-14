a5a6c54ad0569ba9e0432e99172ff9fc2a1b402e
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 13:09:28 -0800

    Implemented destructor for division

--- RPNCalculator/RPNCalculator/DivisionOperation.hpp
+++ RPNCalculator/RPNCalculator/DivisionOperation.hpp
@@ -4,4 +4,5 @@ class DivisionOperator : public AbstractOperation
 	static const char DIVISION_CODE = '/';
 	DivisionOperator() : AbstractOperation(DIVISION_CODE) {};
 	int perform(int a, int b) { return a / b; }
+	~DivisionOperator(){};
 };
