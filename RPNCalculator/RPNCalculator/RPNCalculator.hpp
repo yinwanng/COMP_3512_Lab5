ab4e0ef439f66ecf9b4965a5508b7afd56efc9d2
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 14:16:49 -0800

    Implemented perform function

--- RPNCalculator/RPNCalculator/RPNCalculator.hpp
+++ RPNCalculator/RPNCalculator/RPNCalculator.hpp
@@ -25,6 +25,21 @@ private:
 			break;
 		case '/':
 			return new DivisionOperation;
+			break;
+		}
+	}
+	void perform(Operation * o){
+		//pop top two numbers from stack
+		//apply operation, make sure operator is correct
+		// push back to stack
+		if (stack.size == 2) {
+			int a = stack.top();
+			stack.pop();
+			int b = stack.top();
+			stack.pop();
+			//result = a << o->get_code << b;
+			result = o->perform(a, b);
+			stack.push(result);
 		}
 	}
 };
