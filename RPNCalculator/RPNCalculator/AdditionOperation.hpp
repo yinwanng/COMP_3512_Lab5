2df74fd5c04195810128cdf1b538273a7cf18dae
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 12:51:27 -0800

    Created perform member function

--- RPNCalculator/RPNCalculator/AdditionOperation.hpp
+++ RPNCalculator/RPNCalculator/AdditionOperation.hpp
@@ -4,6 +4,6 @@ class AdditionOperation : public AbstractOperation
 public:
 	static const char ADDITION_CODE = '+';
 	AdditionOperation() : AbstractOperation(ADDITION_CODE) {};
-	
+	int perform(int a, int b) { return a + b; }
 
 };
