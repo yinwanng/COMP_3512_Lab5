6df9ddf3d5ef223cb322e60388e12834516f0a6d
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 13:09:02 -0800

    Implemented perform for division

--- RPNCalculator/RPNCalculator/DivisionOperation.hpp
+++ RPNCalculator/RPNCalculator/DivisionOperation.hpp
@@ -3,4 +3,5 @@ class DivisionOperator : public AbstractOperation
 {
 	static const char DIVISION_CODE = '/';
 	DivisionOperator() : AbstractOperation(DIVISION_CODE) {};
+	int perform(int a, int b) { return a / b; }
 };
