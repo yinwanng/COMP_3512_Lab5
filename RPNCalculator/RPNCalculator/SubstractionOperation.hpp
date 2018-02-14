683c8cdd133994ad885672bf8e3240eb41449239
yinwanng <xxjohnnyng@gmail.com>
2018-02-14 12:59:04 -0800

    Implemented perform function for substraction

--- RPNCalculator/RPNCalculator/SubstractionOperation.hpp
+++ RPNCalculator/RPNCalculator/SubstractionOperation.hpp
@@ -4,5 +4,6 @@ class SubstractionOperation : public AbstractOperation
 public:
 	static const char SUBTRACTION_CODE = '-';
 	SubstractionOperation() : AbstractOperation(SUBTRACTION_CODE) {};
+	int perform(int a, int b) { return a - b; }
 
 };
